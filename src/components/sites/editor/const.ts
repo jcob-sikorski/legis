54807cd36c0732d645a06bf3ff067b62038107ff
bobred18 <envytristan@gmail.com>
2023-11-02 16:59:05 +0100

    adds constants for Editor Components

---
+++ src/components/sites/editor/const.ts
@@ -0,0 +1,15 @@
+export const BAR_WIDTH = 300;
+export const DEV_START_JSON = `[
+{
+    "template_id": "TContact1",
+    "email": "abc@abc.abc",
+    "title": "ABC",
+    "description": "Yep man"
+},
+{
+    "template_id": "TContact3",
+    "email": "def@def.def",
+    "title": "DEF",
+    "description": "Hell nah man"
+}
+]`;
