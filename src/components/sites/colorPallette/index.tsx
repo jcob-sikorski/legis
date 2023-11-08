722e905cf835f96b4183b59f2eb0c83a96660336
jcob-sikorski <jm.siekiera@gmail.com>
2023-11-08 09:48:41 +0100

    fixed a list

--- src/components/sites/colorPallette/index.tsx
+++ src/components/sites/colorPallette/index.tsx
@@ -1,7 +1,7 @@
-import { Layout, Card, Row, Col, Button } from 'antd';
+import { Layout, Card, Row, Col, Button, List } from 'antd';
 
 function ColorPallette() {
-  const groups = ['Group 1', 'Group 2', 'Group 3', 'Group 4', 'Group 5'];
+  const groups = ['Work Ethic', 'Clarity & Problem-solvers', 'Expert & Authority', 'Accessible', 'Gritty', 'Passionate', 'Compassionate & strong', 'Fearless', 'Reliability', 'Loyalty & trust', 'Integrity', 'Excellence', 'Collaboration & working together'];
 
   // Function to generate a random color
   const getRandomColor = () => {
@@ -25,28 +25,32 @@ function ColorPallette() {
       <div style={{ backgroundColor: '#262627' }}>
         <img src="https://ucarecdn.com/194ed0d0-5921-4684-8ae1-02bfd645d41c/_d65e891a18e343b9bccb0adb2a065aca.jpeg" alt="Your description" style={{ width: '100%', maxHeight: '100vh' }} />
       </div>
-      <div style={{ flex: '1', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', overflowY: 'scroll', height: '100vh', paddingTop: '20%'}}>
-        {groups.map((group, index) => (
-          <Card title={group} style={{ width: "80%", backgroundColor: 'transparent', border: 'none' }} key={index}>
-            {[...Array(2)].map((_, i) => (
-              <Row gutter={16} key={i} justify={"center"}>
-                {[...Array(4)].map((_, j) => (
-                  <Col key={j}>
-                    <Button style={{ 
-                        width: 80, 
-                        height: 20, 
-                        borderRadius: 30, 
-                        margin: 10,
-                        background: colors[index*8*2*4 + i*4 + j], 
-                        border: 'none'
-                    }} />
-                  </Col>
-                ))}
-              </Row>
-            ))}
-          </Card>
-        ))}
-      </div>
+      <List
+        style={{ flex: '1', overflowY: 'scroll', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+        dataSource={groups}
+        renderItem={(group, index) => (
+          <List.Item>
+            <Card title={group} style={{ width: "100%", backgroundColor: 'transparent', border: 'none' }}>
+              {[...Array(2)].map((_, i) => (
+                <Row gutter={16} key={i} justify={"center"}>
+                  {[...Array(4)].map((_, j) => (
+                    <Col key={j}>
+                      <Button style={{ 
+                          width: 80, 
+                          height: 20, 
+                          borderRadius: 30, 
+                          margin: 10,
+                          background: colors[index*8*2*4 + i*4 + j], 
+                          border: 'none'
+                      }} />
+                    </Col>
+                  ))}
+                </Row>
+              ))}
+            </Card>
+          </List.Item>
+        )}
+      />
     </Layout>
   );
 }
