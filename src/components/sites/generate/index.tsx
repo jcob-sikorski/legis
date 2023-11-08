50cfe7d4a68f56f99017f2ee94d62a93b42c8803
bobred18 <93402261+bobred18@users.noreply.github.com>
2023-11-08 21:03:29 +0100

    generating 4 section labels to blank page works

--- src/components/sites/generate/index.tsx
+++ src/components/sites/generate/index.tsx
@@ -4,7 +4,8 @@ import { config } from "../../../config";
 import { useEffect, useState } from "react";
 
 import OpenAI from 'openai';
-import { Button } from "antd";
+import { Button, Spin } from "antd";
+import { content, getPrompt } from "./prompt";
 const openai = new OpenAI({
     apiKey: import.meta.env.VITE_REACT_openaiApiKey,
     dangerouslyAllowBrowser: true,
@@ -16,6 +17,9 @@ function Generate() {
     const {site_id} = useParams();
     
     const [onboardingData, setOnboardingData] = useState<any>();
+    const [response, setResponse] = useState<any>();
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<any>();
     const [dataForGeneration, setDataForGeneration] = useState<any>();
   
   const currentUserID = app.currentUser!.id;
@@ -49,12 +53,24 @@ function Generate() {
   }, []);
 
   async function getResponseFromGPT() {
-    const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: 'user', content: 'Say this is a test' }],
+    const prompt = getPrompt(onboardingData);
+
+    setLoading(true);
+      await openai.chat.completions.create({
+        messages: [{ role: 'user', content: prompt }], // 
         model: 'gpt-3.5-turbo',
-      });
-      console.log("chatCompletion: ", chatCompletion)
-      alert("chatCompletion:" + JSON.stringify(chatCompletion))
+        max_tokens: 1000,
+        temperature: 0.3,
+      }).then((res) => {
+        setResponse(JSON.parse(res.choices[0].message.content ?? "{}"))
+        setLoading(false);
+        console.log(res);
+      }).catch((e) => {
+        setError(JSON.stringify(e));
+        console.warn(e);
+      })
+      // console.log("chatCompletion: ", chatCompletion)
+      // alert("chatCompletion:" + JSON.stringify(chatCompletion))
   }
 
     return ( <>
@@ -62,7 +78,48 @@ function Generate() {
         <Button onClick={getResponseFromGPT}>
             getResponseFromGPT()
         </Button>
-        dataForGeneration: {JSON.stringify(dataForGeneration)}
+        <br />
+        <br />
+        onboardingData: {JSON.stringify(onboardingData)}
+        <br />
+
+        {error && <div style={{color: 'red', fontSize: 20}}>ERROR: {error}</div>}
+
+        <h1>AI generated these labels:</h1>
+
+        {loading && <><Spin />Fetching GPT response... </>}
+
+        <h2 style={{fontWeight: 500, fontSize: 25}}>Hero section</h2>
+        <b>
+        <h3><span style={{fontWeight: 300}}>headline:</span> {response?.HeroSection["headline"]}</h3>
+        <h3><span style={{fontWeight: 300}}>sub-headline:</span> {response?.HeroSection["sub-headline"]}</h3>
+        </b>
+
+        <h2 style={{fontWeight: 500, fontSize: 25}}>Practice Areas section</h2>
+        <b>
+        <h3><span style={{fontWeight: 300}}>pageDescription:</span> {response?.PracticeAreas["pageDescription"]}</h3>
+        <h3><span style={{fontWeight: 300}}>values:</span> {response?.PracticeAreas.map((x: any) => <>
+          <h3 style={{marginLeft: 40}}>title: {x?.PracticeAreaTitle}</h3>
+          <h3 style={{marginLeft: 80}}>description: {x?.PracticeAreaDescription}</h3>
+          <br />
+        </>)}</h3>
+        </b>
+
+        <h2 style={{fontWeight: 500, fontSize: 25}}>Core Values section</h2>
+        <b>
+        <h3><span style={{fontWeight: 300}}>pageDescription:</span> {response?.ValuesPage["pageDescription"]}</h3>
+        <h3><span style={{fontWeight: 300}}>values:</span> {response?.ValuesPage["values"]?.map((x: any) => <>
+          <h3 style={{marginLeft: 40}}>Name: {x?.name}</h3>
+          <h3 style={{marginLeft: 80}}>Motto: {x?.motto}</h3>
+          <h3 style={{marginLeft: 120}}>Description: {x?.description}</h3>
+          <br />
+        </>)}</h3>
+        </b>
+
+        <h2 style={{fontWeight: 500, fontSize: 25}}>About Us section</h2>
+        <b>
+        <h3><span style={{fontWeight: 300}}>paragraph:</span> {response?.AboutUsPage["paragraph"]}</h3>
+        </b>
     </> );
 }
 
