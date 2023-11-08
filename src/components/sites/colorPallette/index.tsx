0b4dc021e4ced355333c40bbf0e0f06528b8fd76
jcob-sikorski <jm.siekiera@gmail.com>
2023-11-08 18:21:06 +0100

    extracting the colors from the button clicked

--- src/components/sites/colorPallette/index.tsx
+++ src/components/sites/colorPallette/index.tsx
@@ -1,14 +1,15 @@
 import { Layout, Card, Row, Col, Button, List } from 'antd';
 
 import { valueColorMappping, colorHexMapping } from './colorMappings.tsx'
+import { useState, useEffect } from 'react';
 
 function ColorPallette() {
+  const [buttonColor, setButtonColor] = useState("");
+
   const groups = ['Work Ethic', 'Clarity & Problem-solvers', 'Expert & Authority', 'Accessible', 'Gritty', 'Passionate', 'Compassionate & strong', 'Fearless', 'Reliability', 'Loyalty & trust', 'Integrity', 'Excellence', 'Collaboration & working together'];
 
   // Function to generate a random color
   const getColor = (group: string, buttonNumber: number, i: number, j: number) => {
-    console.log(`group: ${group}, i: ${i}, j: ${j}, buttonNumber: ${buttonNumber}`);
-
     let basicColors = valueColorMappping[group];
 
     let color1 = colorHexMapping[basicColors[0]][buttonNumber];
@@ -16,6 +17,19 @@ function ColorPallette() {
     let color3 = colorHexMapping[basicColors[2]][buttonNumber];
     return `linear-gradient(to right, ${color1} 33%, ${color2} 33%, ${color2} 66%, ${color3} 66%)`;
   }
+
+  const handleClick = (color: string) => {
+    setButtonColor(color);
+  };
+
+  useEffect(() => {
+    if (buttonColor) {
+      const regex = /#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})/g;
+      const matches = buttonColor.match(regex);
+      const uniqueMatches = Array.from(new Set(matches));
+      console.log(uniqueMatches);
+    }
+  }, [buttonColor]);
 
   return (
     <Layout style={{ display: 'flex', flexDirection: 'row', overflow: 'hidden' }}>
@@ -32,14 +46,17 @@ function ColorPallette() {
                 <Row gutter={16} key={i} justify={"center"}>
                   {[...Array(4)].map((_, j) => (
                     <Col key={j}>
-                      <Button style={{ 
-                          width: 80, 
-                          height: 20, 
-                          borderRadius: 30, 
-                          margin: 10,
-                          background: getColor(group, i * 4 + j, i, j),
-                          border: 'none'
-                      }} />
+                      <Button 
+                        onClick={() => handleClick(getColor(group, i * 4 + j, i, j))}
+                        style={{ 
+                            width: 80, 
+                            height: 20, 
+                            borderRadius: 30, 
+                            margin: 10,
+                            background: getColor(group, i * 4 + j, i, j),
+                            border: 'none'
+                        }} 
+                      />
                     </Col>
                   ))}
                 </Row>
