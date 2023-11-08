97095e22aed7905b032e3f51f008eadfb64a8687
jcob-sikorski <jm.siekiera@gmail.com>
2023-11-08 09:41:43 +0100

    fixed the colors on the buttons

--- src/components/sites/colorPallette/index.tsx
+++ src/components/sites/colorPallette/index.tsx
@@ -1,33 +1,31 @@
-import { AlignCenterOutlined } from '@ant-design/icons';
 import { Layout, Card, Row, Col, Button } from 'antd';
-// import React from 'react';
-
-// import "./ColorPallette.css";
 
 function ColorPallette() {
-  const groups = ['Group 1', 'Group 2', 'Group 3', 'Group 4'];
+  const groups = ['Group 1', 'Group 2', 'Group 3', 'Group 4', 'Group 5'];
 
   // Function to generate a random color
   const getRandomColor = () => {
     const letters = '0123456789ABCDEF';
     let color1 = '#';
     let color2 = '#';
+    let color3 = '#';
     for (let i = 0; i < 6; i++) {
       color1 += letters[Math.floor(Math.random() * 16)];
       color2 += letters[Math.floor(Math.random() * 16)];
+      color3 += letters[Math.floor(Math.random() * 16)];
     }
-    return `linear-gradient(to right, ${color1} 33%, ${color2} 33%, ${color2} 66%, ${color1} 66%)`;
+    return `linear-gradient(to right, ${color1} 33%, ${color2} 33%, ${color2} 66%, ${color3} 66%)`;
   }
 
   // Create an array of random colors
-  const colors = Array.from({length: 3*32}, getRandomColor);
+  const colors = Array.from({length: 8*2*4*groups.length}, getRandomColor);
 
   return (
     <Layout style={{ display: 'flex', flexDirection: 'row', overflow: 'hidden' }}>
       <div style={{ backgroundColor: '#262627' }}>
         <img src="https://ucarecdn.com/194ed0d0-5921-4684-8ae1-02bfd645d41c/_d65e891a18e343b9bccb0adb2a065aca.jpeg" alt="Your description" style={{ width: '100%', maxHeight: '100vh' }} />
       </div>
-      <div style={{ flex: '1', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+      <div style={{ flex: '1', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', overflowY: 'scroll', height: '100vh', paddingTop: '20%'}}>
         {groups.map((group, index) => (
           <Card title={group} style={{ width: "80%", backgroundColor: 'transparent', border: 'none' }} key={index}>
             {[...Array(2)].map((_, i) => (
@@ -39,7 +37,7 @@ function ColorPallette() {
                         height: 20, 
                         borderRadius: 30, 
                         margin: 10,
-                        background: colors[i*4+j], 
+                        background: colors[index*8*2*4 + i*4 + j], 
                         border: 'none'
                     }} />
                   </Col>
