00f1bd7e5ef0f02073cc93cecc5245d4aa1628be
jcob-sikorski <jm.siekiera@gmail.com>
2023-11-09 18:08:01 +0100

    added a link to the menu

--- src/components/sites/menu/index.tsx
+++ src/components/sites/menu/index.tsx
@@ -1,5 +1,6 @@
 import { Layout, Menu } from 'antd';
 import { AppstoreOutlined, BarChartOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
 const { Sider } = Layout;
 
@@ -10,12 +11,14 @@ const App = () => (
       style={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingLeft: 4 }}
     >
       {[
-        { key: '1', icon: <AppstoreOutlined style={{ fontSize: '24px' }}/> },
+        { key: '1', icon: <AppstoreOutlined style={{ fontSize: '24px' }}/>, to: '/dashboard' },
         { key: '2', icon: <BarChartOutlined style={{ fontSize: '24px' }}/> },
         { key: '3', icon: <SettingOutlined style={{ fontSize: '24px' }}/> },
         { key: '4', icon: <UserOutlined style={{ fontSize: '24px' }}/> },
       ].map(item => (
-        <Menu.Item key={item.key} icon={item.icon} style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }} />
+        <Menu.Item key={item.key} icon={item.icon} style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Link to={item.to as string}/>
+        </Menu.Item>
       ))}
     </Menu>
   </Sider>
