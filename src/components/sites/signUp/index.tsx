e45ac4d229fb1eb493d96f454869614c34e455cc
bobred18 <93402261+bobred18@users.noreply.github.com>
2023-11-17 00:03:55 +0100

    minor fix to loops testing

--- src/components/sites/signUp/index.tsx
+++ src/components/sites/signUp/index.tsx
@@ -4,6 +4,8 @@ import { useApp } from "../../RealmApp";
 import * as Realm from "realm-web";
 import { message } from 'antd';
 import "./index.css";
+import { config } from "../../../config";
+import axios from "axios";
 
 export default function SignUp() {
   const app: any = useApp();
@@ -13,17 +15,36 @@ export default function SignUp() {
   const navigate = useNavigate();
 
   const handleSignUp = async () => {
-    // app.logOut();
-    if (email && email.length < 80 && password && password.length >= 6 && password.length <= 100) {
+    if (true || email && email.length < 80 && password && password.length >= 6 && password.length <= 100) {
       try {
+        // EMAIL FOR TESTING IS CHANGED TEMPORARILY HERE
+        const email = "envytristan@gmail.com";
         // Register new email/password user
-        await app.emailPasswordAuth.registerUser({email, password});
-  
-        // TODO send welcome email with the authentication and wait for the confirmation before log in the user
+        // await app.emailPasswordAuth.registerUser({email, password: "123456"});
   
         // Log in the email/password user
-        await app.logIn(Realm.Credentials.emailPassword(email, password));
-        navigate('/');
+        await app.logIn(Realm.Credentials.emailPassword(email, "123456"));
+
+        // TODO send welcome email
+        // navigate(`/${email}`);
+
+        const data = {
+          email: email,
+          eventName: "welcomeUser"
+        };
+        
+        const tsx = {
+          method: 'post',
+          url: 'https://app.loops.so/api/v1/events/send',
+          headers: { 
+            Authorization: `Bearer ${config.loopsKey}`
+          },
+          data : data
+        };
+        
+        const response = await axios(tsx);
+
+        console.log("LOOPS RESPONSE: ", (response));
         
         setTimeout(() => {
           setEmail("");
