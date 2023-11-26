a4fdcf0576b69dc17c670ee5231eb8dcc09566be
jcob-sikorski <jm.siekiera@gmail.com>
2023-11-26 17:05:13 +0100

    navigating to the site-settings from the hosting custom domain UI

--- src/components/sites/customDomainDeployment/addSiteDetails.tsx
+++ src/components/sites/customDomainDeployment/addSiteDetails.tsx
@@ -1,10 +1,38 @@
 import { Layout, Typography, Button, Steps, Input } from 'antd';
-import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { useDispatch } from 'react-redux';
+import { setSite } from '../../../redux/actions';
+
+import { useApp } from '../../RealmApp';
+import * as Realm from "realm-web";
 
 const { Step } = Steps;
 const { Title } = Typography;
 
-function AddSiteDetails({ nextPage }: any) {
+function AddSiteDetails() {
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const { site_id } = useParams();
+
+  const app: any = useApp();
+
+  const mongodb = app.currentUser!.mongoClient("mongodb-atlas");  
+  const site_collection = mongodb.db("legis").collection("Site");
+
+  async function onContinue() {
+    const site = await site_collection.findOne(
+      { _id: new Realm.BSON.ObjectId(site_id) },
+      { projection: { cname: 1 } }
+    );
+
+    dispatch(setSite(site));
+
+    navigate('/overview-settings')
+}
+
   return (
     <Layout style={{ height: '100vh', backgroundColor: 'white' }}>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white', marginTop: '50px' }}>
@@ -21,7 +49,7 @@ function AddSiteDetails({ nextPage }: any) {
       <h2 style={{ fontSize: 18, fontWeight: '300', wordWrap: 'break-word', lineHeight: '20px', textAlign: 'center', alignSelf: 'center' }}>your new website. Simply click the button below and go to your</h2>
       <h2 style={{ fontSize: 18, fontWeight: '300', wordWrap: 'break-word', lineHeight: '20px', textAlign: 'center', alignSelf: 'center' }}>“site settings”</h2>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <Button onClick={() => nextPage()}  className="custom-button" type="primary" size="large" style={{ marginTop: 50 }}>
+        <Button onClick={onContinue}  className="custom-button" type="primary" size="large" style={{ marginTop: 50 }}>
           Go to site settings
         </Button>
       </div>
