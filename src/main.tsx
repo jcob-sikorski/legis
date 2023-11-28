dcd57f82aad403fa0907b06bf20dfbdc6c646ed5
bobred18 <93402261+bobred18@users.noreply.github.com>
2023-11-28 21:55:15 +0100

    adds redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
 
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { persistReducer, persistStore } from 'redux-persist';
+
 // CSS
 import './index.css'
 
@@ -10,26 +14,38 @@ import { Provider as ReduxProvider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './redux/reducers/index.ts'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({ 
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 })
 
+const persistor = persistStore(store);
+
 // ANTDesign
-import { ConfigProvider, Typography } from "antd";
+import { ConfigProvider, Spin, Typography } from "antd";
+// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
     <ReduxProvider store={store}>
-      <ConfigProvider
-        theme={{
-          token: {
-            fontFamily: "Poppins"
-          }
-        }}
-      >
-        <App />
-      </ConfigProvider>
+      <PersistGate loading={<Spin size='large' />} persistor={persistor}>
+        <ConfigProvider
+          theme={{
+            token: {
+              fontFamily: "Poppins"
+            }
+          }}
+          >
+          <App />
+        </ConfigProvider>
+      </PersistGate>
     </ReduxProvider>
   // </React.StrictMode>,
 )
